9879630f09091d915d853dbf74cab95c559d8c95
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-09-30 21:25:54 -0300

    chore: configure store dynamic

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,18 +1,25 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { Action, combineReducers, configureStore, PreloadedState, ThunkAction } from '@reduxjs/toolkit';
 import publishersReducer, { publishersApiSlice } from '../features/publishers/publisherSlice';
 import { apiSlice } from '../features/api/apiSlice';
 
-export const store = configureStore({
-  reducer: {
-    publishers: publishersReducer,
-    [apiSlice.reducerPath]: apiSlice.reducer,
-    [publishersApiSlice.reducerPath]: publishersApiSlice.reducer
-  },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware)
-});
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+const rootReducer = combineReducers({
+  publishers: publishersReducer,
+  [apiSlice.reducerPath]: apiSlice.reducer,
+  [publishersApiSlice.reducerPath]: publishersApiSlice.reducer
+})
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware)
+  })
+}
+
+export type AppStore = ReturnType<typeof setupStore>
+export type AppDispatch = AppStore['dispatch'];
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType,
   RootState,
   unknown,
