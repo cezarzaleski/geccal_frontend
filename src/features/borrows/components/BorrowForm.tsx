2f06230a3de28a7eef4b139be47bc098789f68a2
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2024-01-06 21:41:32 -0300

    feat(BorrowForm): add evangelizandos and borrow at fields

--- src/features/borrows/components/BorrowForm.tsx
+++ src/features/borrows/components/BorrowForm.tsx
@@ -1,6 +1,13 @@
-import { Box, FormControl, Grid } from '@mui/material';
+import { Box, Button, FormControl, Grid } from '@mui/material';
 import React from 'react';
 import { AuCompleteCustom } from '../../../components/AuCompleteCustom';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
+import { Link } from 'react-router-dom';
+
+dayjs.locale('pt-br');
 
 
 type Props = {
@@ -28,13 +35,13 @@ export function BorrowForm(
 
   const handleChangeWithValidation = (e: React.ChangeEvent<HTMLInputElement>) => {
     handleChange(e);
-    let tempErrors = { ...errors };
+    let tempErrors = {...errors};
     tempErrors[e.target.name] = e.target.value ? "" : "Campo obrigatório";
     setErrors(tempErrors);
   };
 
   const validate = () => {
-    let tempErrors = { ...errors };
+    let tempErrors = {...errors};
     setErrors(tempErrors);
     return Object.values(tempErrors).every(value => value === "");
   };
@@ -49,19 +56,59 @@ export function BorrowForm(
 
   return (
     <form onSubmit={handleSubmitWithValidation}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Box mb={2}>
+            <FormControl fullWidth>
+              <AuCompleteCustom
+                name="bookId"
+                label="Livro"
+                isLoading={isLoading}
+                isDisabled={isDisabled}
+                values={borrow.bookId}
+                options={[]}
+                handleChange={handleChange}
+              />
+            </FormControl>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Box mb={2}>
+            <FormControl fullWidth>
+              <AuCompleteCustom
+                name="bookId"
+                label="Livro"
+                isLoading={isLoading}
+                isDisabled={isDisabled}
+                values={borrow.bookId}
+                options={[]}
+                handleChange={handleChange}
+              />
+            </FormControl>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Box mb={2}>
+            <FormControl fullWidth>
+              <LocalizationProvider dateAdapter={AdapterDayjs} dateLibInstance={dayjs}>
+                <DatePicker label="Data de Empréstimo" value={dayjs()}/>
+              </LocalizationProvider>
+            </FormControl>
+          </Box>
+        </Grid>
+      </Grid>
       <Grid item xs={12}>
-        <Box mb={2}>
-          <FormControl fullWidth>
-            <AuCompleteCustom
-              name="bookId"
-              label="Livro"
-              isLoading={isLoading}
-              isDisabled={isDisabled}
-              values={borrow.bookId}
-              options={[]}
-              handleChange={handleChange}
-            />
-          </FormControl>
+        <Box display="flex" gap={2}>
+          <Button variant="contained" component={Link} to="/emprestimos" color="secondary">
+            Voltar
+          </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            disabled={isDisabled}
+          >
+            Salvar
+          </Button>
         </Box>
       </Grid>
     </form>
